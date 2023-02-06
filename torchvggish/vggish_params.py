ded7161d14488b1aceb69369e2fb26e05a673085
Promit Basak <basakpromit@gmail.com>
2023-02-06 15:22:14 +0600

    Changed parameters

--- torchvggish/vggish_params.py
+++ torchvggish/vggish_params.py
@@ -19,19 +19,19 @@ See vggish_slim.py for more information.
 """
 
 # Architectural constants.
-NUM_FRAMES = 96  # Frames in input mel-spectrogram patch.
+NUM_FRAMES = 96*3  # Frames in input mel-spectrogram patch.
 NUM_BANDS = 64  # Frequency bands in input mel-spectrogram patch.
 EMBEDDING_SIZE = 128  # Size of embedding layer.
 
 # Hyperparameters used in feature and example generation.
-SAMPLE_RATE = 16000
+SAMPLE_RATE = 8000
 STFT_WINDOW_LENGTH_SECONDS = 0.025
 STFT_HOP_LENGTH_SECONDS = 0.010
 NUM_MEL_BINS = NUM_BANDS
 MEL_MIN_HZ = 125
 MEL_MAX_HZ = 7500
 LOG_OFFSET = 0.01  # Offset used for stabilized log of input mel-spectrogram.
-EXAMPLE_WINDOW_SECONDS = 0.96  # Each example contains 96 10ms frames
+EXAMPLE_WINDOW_SECONDS = 0.96  # Each example contains 96*3 = 288 10ms frames
 EXAMPLE_HOP_SECONDS = 0.96  # with zero overlap.
 
 # Parameters used for embedding postprocessing.
