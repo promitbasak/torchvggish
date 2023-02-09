1d8d046bb67fdc9de46581f701ed1856bc418717
Promit Basak <basakpromit@gmail.com>
2023-02-09 22:12:48 +0600

    Changed parameters

--- torchvggish/vggish_params.py
+++ torchvggish/vggish_params.py
@@ -31,8 +31,8 @@ NUM_MEL_BINS = NUM_BANDS
 MEL_MIN_HZ = 125
 MEL_MAX_HZ = 3000
 LOG_OFFSET = 0.01  # Offset used for stabilized log of input mel-spectrogram.
-EXAMPLE_WINDOW_SECONDS = 9.2  # Each example contains 96*3 = 288 10ms frames
-EXAMPLE_HOP_SECONDS = 9.2  # with zero overlap.
+EXAMPLE_WINDOW_SECONDS = 0.96  # Each example contains 96*3 = 288 10ms frames
+EXAMPLE_HOP_SECONDS = 0.96  # with zero overlap.
 
 # Parameters used for embedding postprocessing.
 PCA_EIGEN_VECTORS_NAME = 'pca_eigen_vectors'
